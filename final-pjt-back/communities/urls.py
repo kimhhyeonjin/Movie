618ac488c776ad65ee5b3b97e245eb3aedd20ed6
ahyunnn <ahyun3089@gmail.com>
2022-11-18 16:01:42 +0900

    1118 communities views.py 수정

--- final-pjt-back/communities/urls.py
+++ final-pjt-back/communities/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('articles/', views.article_list),
     # 게시글 세부 정보 조회, 수정, 삭제
     path('articles/<int:article_pk>/', views.article_detail),
+    # 댓글 생성
+    path('articles/<int:article_pk>/create_comment/', views.create_comment),
     # 댓글 목록 조회
-    path('comments/', views.comment_list),
+    path('articles/<int:article_pk>/comments/', views.comment_list),
     # 댓글 세부 정보 조회, 수정, 삭제
     path('comments/<int:comment_pk>/', views.comment_detail),
-    # 댓글 생성
-    path('articles/<int:article_pk>/comments/', views.comment_create),
 ]
--- final-pjt-back/communities/views.py
+++ final-pjt-back/communities/views.py
@@ -49,13 +49,13 @@ def article_detail(request, article_pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
-def comment_list(request):
-    comments = get_list_or_404(Comment)
+def comment_list(request, article_pk):
+    comments = get_list_or_404(Comment, article=article_pk)
     serializer = CommentListSerializer(comments, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-def comment_create(request, article_pk):
+def create_comment(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     serializer = CommentDetailSerializer(data = request.data)
     if serializer.is_valid(raise_exception=True):
