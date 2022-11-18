a153d49d31f3183a607a5835587781c1ae29a3a9
kimhhyeonjin <kimhhyeonjin@gmail.com>
2022-11-18 16:41:14 +0900

    MovieList 포스터 추가

--- final-pjt-front/src/components/PopularListView.vue
+++ final-pjt-front/src/components/PopularListView.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <!-- <h3>PopularListView</h3> -->
+    <img
+      :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`"
+      :alt="`${movie.title}_poster`"
+      width="180px"
+      height="260px"
+    >
+    <br>
     {{ movie.title }}
   </div>
 </template>
--- final-pjt-front/src/components/ReviewList.vue
+++ final-pjt-front/src/components/ReviewList.vue
@@ -1,17 +1,28 @@
 <template>
-  <h3>ReviewList</h3>
+  <div>
+    <h3>ReviewList</h3>
+    <ReviewListItem
+      v-for="review in reviews"
+      :key="review.id"
+      :review="review"
+    />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ReviewListItem from '@/components/ReviewListItem'
 
 const API_URL = 'http://127.0.0.1:8000'
 
 export default {
   name: 'ReviewList',
+  components: {
+    ReviewListItem
+  },
   data() {
     return {
-      review: null,
+      reviews: []
     }
   },
   created() {
@@ -19,13 +30,15 @@ export default {
   },
   methods: {
     getReviewList() {
-      console.log(this.$route.params.movie_id)
+      console.log(this.reviews)
       axios({
         method: 'get',
         url: `${API_URL}/movies/${this.$route.params.movie_id}/reviews/`
       })
         .then((response) => {
           console.log(response)
+          const reviews = response.data
+          this.reviews = reviews
         })
         .catch((error) => {
           console.log(error)
