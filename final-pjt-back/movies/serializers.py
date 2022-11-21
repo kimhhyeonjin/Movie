cc1e33b1503d95a8849fc2c266ae75c6cf94353c
ahyunnn <ahyun3089@gmail.com>
2022-11-22 02:32:26 +0900

    1122 random기능추가

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from .models import Movie, Genre, Review
 
-class MovieListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Movie
-        fields = ('id', 'title', 'overview', 'poster_path', 'vote_average')
-
 class MovieDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
         fields = '__all__'
         read_only_fields = ('genre_ids', 'like_users',)
-        
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('id', 'name',)
+
+class MovieListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'overview', 'poster_path', 'vote_average')
 
 class ReviewSerializer(serializers.ModelSerializer):
 
@@ -21,9 +26,3 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = '__all__'
         read_only_fields = ('movie', 'user',)
-
-class GenreSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Genre
-        fields = ('id', 'name',)
