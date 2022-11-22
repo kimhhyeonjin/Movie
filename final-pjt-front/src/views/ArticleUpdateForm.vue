4a278115994c7b185b5d5a9da14ff8fb03944768
kimhhyeonjin <kimhhyeonjin@gmail.com>
2022-11-22 20:07:24 +0900

    1122 게시글 수정 기능 추가

--- final-pjt-front/src/views/ArticleUpdateForm.vue
+++ final-pjt-front/src/views/ArticleUpdateForm.vue
@@ -7,6 +7,7 @@
       <label for="content">내용 : </label>
       <textarea id="content" cols="30" rows="10" v-model.trim="content"></textarea><br>
       <input id="submit" type="submit">
+      <!-- {{ article }} -->
     </form>
   </div>
 </template>
@@ -26,34 +27,51 @@ export default {
       articleupdate : false,
     }
   },
+  computed: {
+    article() {
+      return this.$store.state.article
+    }
+  },
   methods: {
-    loadArticle() {
-      if (this.articleupdate === false) {
-        this.articleupdate = true
-      } else {
-        const articleObj = {
-          'title' : this.title,
-          'content' : this.content
-        }
-        axios({
-          method: 'get',
-          url : `${API_URL}/communities/articles/${this.$route.params.article_id}`,
-          data: {
-            'articleObj' : articleObj
-          }
-        })
-          .then((response) => {
-            console.log(response)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-        this.articleupdate = false
-      }
+    getArticleDetail() {
+      this.$store.dispatch('getArticleDetail', `${this.$route.params.article_id}`)
+      this.title = this.article.title
+      this.content = this.article.content
     },
+    updateArticle() {
+      const title = this.title
+      const content = this.content
+      if (!title) {
+        alert('제목을 입력해주세요')
+      } else if (!content) {
+        alert('내용을 입력해주세요')
+        return
+      }
+      axios({
+        method: 'put',
+        url: `${API_URL}/communities/articles/${this.$route.params.article_id}/`,
+        data: {
+          title: title,
+          content: content,
+          // user는 django에서 user_id를 뜻함
+          user: this.$store.state.user.pk
+        },
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`
+        }
+      })
+        .then((response) => {
+          console.log(response)
+          this.$router.push({ name: 'ArticleDetail', params: `${this.$route.params.article_id}`})
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+
+    }
   },
   created() {
-    this.loadArticle()
+    this.getArticleDetail()
   },
 }
 </script>
