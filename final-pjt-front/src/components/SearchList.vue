67ef8b804aca360da1f229567ad9caf716d3b348
ahyun3089 <ahyun3089@naver.com>
2022-11-22 14:47:13 +0900

    1122 마이페이지 수정중

--- final-pjt-front/src/components/SearchList.vue
+++ final-pjt-front/src/components/SearchList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <!-- <h2>SearchList</h2> -->
-    <p>{{ result.title }}</p>
+    <div @click="goMovieDetail(result.id)">
+      <img
+        :src="`https://image.tmdb.org/t/p/original${result.poster_path}`"
+        :alt="`${result.title}_poster`"
+        width="180px"
+        height="260px"
+      >
+      <p>{{ result.title }}</p>
+      <!-- <p>{{ result.id }}</p> -->
+
+    </div>
   </div>
 </template>
 
@@ -10,6 +20,11 @@ export default {
   name: 'SearchList',
   props: {
     result: Object,
+  },
+  methods: {
+    goMovieDetail(movie_id) {
+      this.$router.push({ name: 'MovieDetail', params: {movie_id}})
+    }
   }
 }
 </script>
