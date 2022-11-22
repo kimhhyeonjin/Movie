82971eb20b6488f309723d1684e6a0f140246f0f
kimhhyeonjin <kimhhyeonjin@gmail.com>
2022-11-23 00:02:54 +0900

    1123 마이페이지 구현

--- final-pjt-front/src/views/MypageView.vue
+++ final-pjt-front/src/views/MypageView.vue
@@ -3,19 +3,22 @@
     <h1>Mypage</h1>
     <br>
     <!-- 자기자신을 팔로우하지 않도록 v-show 이용 -->
-    <form @submit.prevent="followUser">
+    <form v-show="followMe" @submit.prevent="followUser">
       <input v-show="isFollow" type="submit" value="팔로우 취소">
       <input v-show="!isFollow" type="submit" value="팔로우">
     </form>
     <br>
     <br>
     <h5>name : {{ username }}</h5>
+    <br>{{ userdata }}
+    <br>
     <br>
     <h5>내가 좋아요한 영화</h5>
+    <h5>{{ userdata.like_movies }}</h5>
     <br>
-    <h5>팔로잉 : </h5>
+    <h5>팔로잉 : {{ userdata.followings }}</h5>
     <br>
-    <h5>팔로워 : </h5>
+    <h5>팔로워 : {{ userdata.followers }}</h5>
   </div>
 </template>
 
@@ -28,30 +31,81 @@ export default {
   name: 'MypageView',  
   data() {
     return {
+      userdata: null,
       username: this.$route.params.username,
-      isFollow: null,
+      isFollow: false,
+      followMe: false,
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.state.user
     }
   },
   methods: {
+    isFollowMe() {
+      if (this.username === this.user.username ) {
+        this.followMe = false
+      } else {
+        this.followMe = true
+      }
+    },
     followUser() {
       axios({
-        method: 'post',
-        url: `${API_URL}/accounts/like/${this.$route.params.username}/follow/`,
+        method: 'get',
+        url: `${API_URL}/accounts/like/getuserid/${this.$route.params.username}/`,
         headers: {
           Authorization: `Token ${this.$store.state.token}`
         },
       })
         .then((response) => {
           console.log(response)
-          this.isFollow = response.data.isFollow
+          this.userdata = response.data
+          const id = response.data.id
+          axios({
+            method: 'post',
+            url: `${API_URL}/accounts/like/${id}/follow/`,
+            headers: {
+              Authorization: `Token ${this.$store.state.token}`
+            },
+          })
+            .then((response) => {
+              console.log(response)
+              this.isFollow = response.data.isFollow
+            })
+            .catch((error) => {
+              console.log(error)
+            })
         })
         .catch((error) => {
           console.log(error)
         })
-    }
+    },
+    userData() {
+      axios({
+        method: 'get',
+        url: `${API_URL}/accounts/like/getuserid/${this.$route.params.username}/`,
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`
+        },
+      })
+        .then((response) => {
+          console.log(response)
+          this.userdata = response.data
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+  created() {
+    this.isFollowMe()
+    this.userData()
   },
   beforeRouteUpdate(to, from, next){
     this.username = to.params.username
+    this.followMe = false
     next()
   },
 }
